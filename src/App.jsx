126ee9940ebb57e1377ea082d29820a175a5fda4
Hugo Moya <133818289+hema1019@users.noreply.github.com>
2023-07-06 20:21:21 -0600

    empty project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Rick and Morty App</h1>
+    </div>
+  )
+}
+
+export default App
